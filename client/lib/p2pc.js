c780212acbd6f7e4f14821601c5e5c88d4e812c3
Gábor Molnár <gabor.molnar@sch.bme.hu>
2011-11-22 20:15:34 +0100

    Chat próbaalkalmazás a böngészők közti kommunikáció tesztelésére.

--- client/lib/p2pc.js
+++ client/lib/p2pc.js
@@ -1,8 +1,10 @@
 (function() {
-  var main, worker;
+  var hookjs_test, main, worker;
 
   main = function() {
     var worker;
+    window.onload = hookjs_test;
+    return;
     console.log('main');
     worker = new SharedWorker("/p2pc.js");
     worker.onerror = function(e) {
@@ -12,6 +14,51 @@
       return console.log(e.data);
     };
     return worker.port.start();
+  };
+
+  hookjs_test = function() {
+    var Hook, chat, checkSubmit, input, messages, name, submitMessage;
+    chat = document.createElement('div');
+    chat.setAttribute('style', 'position: fixed;\nright: 2em;\ntop: 1em;\nbottom: 1em;\n\nwidth: 20em;\nmargin-bottom: 2em;\n\nbackground-color: rgba(0,0,0,0.1);\ncolor: black;');
+    document.body.appendChild(chat);
+    messages = document.createElement('div');
+    messages.setAttribute('style', 'position: relative;\nheight: 100%;\n-bottom: 2em;\noverflow-y: scroll;');
+    chat.appendChild(messages);
+    input = document.createElement('input');
+    input.setAttribute('style', 'font-size: 1.2em;\n\nposition: absolute;\nleft: 0;\nright: 0;\nbottom: -2em;\n\nheight: 1.5em;\nmargin: 0;\npadding: 0;\n\nbackground-color: rgba(255,255,255,0.2);');
+    chat.appendChild(input);
+    Hook = window.require('/hook.js').Hook;
+    window.hook = new Hook();
+    window.hook.connect();
+    name = 'browser-' + Math.floor(Math.random() * 100);
+    setTimeout((function() {
+      return hook.emit(name + '::name', name);
+    }), 500);
+    window.hook.on('*::message', function(message) {
+      var node;
+      node = document.createTextNode("" + message.from + " - " + message.content);
+      messages.appendChild(node);
+      return messages.appendChild(document.createElement('br'));
+    });
+    window.hook.on('*::name', function(message) {
+      var message_node;
+      message_node = document.createTextNode(message + ' opened the page.');
+      messages.appendChild(message_node);
+      return messages.appendChild(document.createElement('br'));
+    });
+    submitMessage = function(message) {
+      return hook.emit(name + '::message', {
+        from: name,
+        content: message
+      });
+    };
+    checkSubmit = function(e) {
+      if (e.keyCode === 13) {
+        submitMessage(input.value);
+        return input.value = '';
+      }
+    };
+    return input.addEventListener('keydown', checkSubmit, false);
   };
 
   worker = function() {
--- client/src/main.coffee
+++ client/src/main.coffee
@@ -1,4 +1,7 @@
 main = ->
+  window.onload = hookjs_test
+  return
+
   console.log 'main'
 
   worker = new SharedWorker "/p2pc.js"
@@ -10,3 +13,74 @@ main = ->
     console.log e.data
 
   worker.port.start()
+
+hookjs_test = ->
+  chat = document.createElement 'div'
+  chat.setAttribute 'style', '''
+    position: fixed;
+    right: 2em;
+    top: 1em;
+    bottom: 1em;
+
+    width: 20em;
+    margin-bottom: 2em;
+
+    background-color: rgba(0,0,0,0.1);
+    color: black;
+  '''
+  document.body.appendChild chat
+
+  messages = document.createElement 'div'
+  messages.setAttribute 'style', '''
+    position: relative;
+    height: 100%;
+    -bottom: 2em;
+    overflow-y: scroll;
+  '''
+  chat.appendChild messages
+
+  input = document.createElement 'input'
+  input.setAttribute 'style', '''
+    font-size: 1.2em;
+
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: -2em;
+
+    height: 1.5em;
+    margin: 0;
+    padding: 0;
+
+    background-color: rgba(255,255,255,0.2);
+  '''
+  chat.appendChild input
+
+  Hook = window.require('/hook.js').Hook
+  window.hook = new Hook()
+  window.hook.connect()
+
+  name = 'browser-' + Math.floor(Math.random()*100)
+  setTimeout (-> hook.emit name + '::name', name), 500
+
+  window.hook.on '*::message', (message) ->
+    node = document.createTextNode("#{message.from} - #{message.content}")
+    messages.appendChild node
+    messages.appendChild document.createElement 'br'
+
+  window.hook.on '*::name', (message) ->
+    message_node = document.createTextNode(message + ' opened the page.')
+    messages.appendChild message_node
+    messages.appendChild document.createElement 'br'
+
+  submitMessage = (message) ->
+    hook.emit name + '::message',
+      from : name
+      content : message
+
+  checkSubmit = (e) ->
+    if e.keyCode is 13
+      submitMessage(input.value)
+      input.value = ''
+
+  input.addEventListener('keydown', checkSubmit, false);
